ee80eaf6a2543738cdfef987bb1847742147286a
Minajeanne <albrown07@mac.com>
2020-02-03 19:11:12 -0500

    update packages - fix core-js

--- src/components/container.js
+++ src/components/container.js
@@ -3,4 +3,3 @@ import React from 'react'
 export default ({ children }) => (
   <div style={{ maxWidth: 1180, margin: '0 auto' }}>{children}</div>
 )
-// CFPAT-LhNYWiKlVWfSgjSJWBAPc6KjImhyUOfwd1b9jrWwr6s
