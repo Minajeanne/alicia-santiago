425938a36ecb02694142db5cad0bb36ca39e362e
Minajeanne <albrown07@mac.com>
2020-02-05 11:21:43 -0500

    add image

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,16 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
-    }
+      // resolve: `gatsby-plugin-manifest`,
+      // options: {
+      //   name: `GatsbyJS`,
+      //   short_name: `GatsbyJS`,
+      //   start_url: `/`,
+      //   background_color: `#f7f0eb`,
+      //   theme_color: `#a2466c`,
+      //   display: `standalone`,
+      //   icon: `src/images/icon.png`,
+      // },
+    },
   ],
 }
